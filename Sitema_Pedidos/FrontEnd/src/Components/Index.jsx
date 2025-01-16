cba36f77796ed1e80c2972d18b87177f1cfcf8a0
Bruno de Fraga <brunofragaa97@gmail.com>
2025-01-16 11:45:57 -0300

    adicionado modal ao clicar no botao cadastro, modal ja ajustado

--- Sitema_Pedidos/FrontEnd/src/Components/Index.jsx
+++ Sitema_Pedidos/FrontEnd/src/Components/Index.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
+import Modal from "./Modal";
 import "../../styles/Cardapio.css";
 import CadastroCliente from "./CadastroCliente";
 import Pedido from "./Pedido";
 
 const Index = () => {
+  const [activeModalContent, setActiveModalContent] = useState(null);
+
+  // Função para abrir o modal
+  const openModal = (index) => {
+    setActiveModalContent(index);
+  };
+
+  // Função para fechar o modal
+  const closeModal = () => {
+    setActiveModalContent(null);
+  };
+
   return (
     <div className="background-container">
       <div className="button-container">
-        <button>CADASTRAR</button>
+        <button onClick={() => openModal(1)}>CADASTRAR</button>
         <button>ENTRAR</button>
+        <Modal isOpen={activeModalContent === 1} onClose={closeModal}>
+          
+            <CadastroCliente />
+         
+        </Modal>
       </div>
       <Pedido />
     </div>
