5ca23f97a9dab6aebd90b595953917733cd6fd10
Bruno de Fraga <brunofragaa97@gmail.com>
2025-01-16 11:17:24 -0300

    adicionado botao cadastrar e logar

--- Sitema_Pedidos/FrontEnd/src/Components/Index.jsx
+++ Sitema_Pedidos/FrontEnd/src/Components/Index.jsx
@@ -1,13 +1,17 @@
 import "../../styles/Cardapio.css";
+import CadastroCliente from "./CadastroCliente";
 import Pedido from "./Pedido";
 
 const Index = () => {
   return (
-  <div className="background-container">
-    <Pedido />
-
-  </div>
-  )
+    <div className="background-container">
+      <div className="button-container">
+        <button>CADASTRAR</button>
+        <button>ENTRAR</button>
+      </div>
+      <Pedido />
+    </div>
+  );
 };
 
 export default Index;
